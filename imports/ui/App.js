033b715c5a1689611e96d4a8e3db6f9c29449744
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-11-27 11:59:58 -0500

    change

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <TitleBar title={this.props.title} subtitle="Created by Andrew Mead"/>
+        <TitleBar title={this.props.title} subtitle="Created by Daniel GG"/>
         <div className="wrapper">
           <PlayerList players={this.props.players}/>
           <AddPlayer/>
