77e45d3dc60938cccb09f8cc450c1e105c6da419
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-11-27 11:33:24 -0500

    change to PropTypes

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -5,22 +5,21 @@ import TitleBar from './TitleBar';
 import AddPlayer from './AddPlayer';
 import PlayerList from './PlayerList';
 
-
 export default class App extends React.Component {
-    render(){
-        return (
-            <div>              
-                    <TitleBar title={this.props.title} subtitle="Created by Daniel GG"/>            
-             <div className= "wrapper">
-                    <PlayerList players={this.props.players}/>
-                    <AddPlayer/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <TitleBar title={this.props.title} subtitle="Created by Andrew Mead"/>
+        <div className="wrapper">
+          <PlayerList players={this.props.players}/>
+          <AddPlayer/>
+        </div>
+      </div>
+    );
+  }
 };
 
 App.propTypes = {
-    title: PropTypes.string.isRequired,
-    players: PropTypes.array.isRequired
-}
+  title: PropTypes.string.isRequired,
+  players: PropTypes.array.isRequired
+};
--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,38 +1,34 @@
 import React from 'react';
-import Player from './Player';
-import PropTypes from 'prop-types';
 import FlipMove from 'react-flip-move';
+import PropTypes from 'prop-types';
 
+import Player from './Player';
 
 export default class PlayerList extends React.Component {
-    renderPlayers(){
-        if(this.props.players.length === 0){
-            return (
-                <div className="item">
-             <p className="item__message"> Add a new Player to get Started</p>
-             </div>
-            );
-
-        }else{
-              return this.props.players.map((player)=> {
-            return <Player key={player._id} player={player}/>;
-            }); 
-        }
-      
+  renderPlayers() {
+    if (this.props.players.length === 0) {
+      return (
+        <div className="item">
+          <p className="item__message">Add your first player to get started!</p>
+        </div>
+      );
+    } else {
+      return this.props.players.map((player) => {
+        return <Player key={player._id} player={player}/>;
+      });
     }
-    render() {
-        return (
-            
-            <div>
-                <FlipMove duration={750} easing="ease-out">
-                {this.renderPlayers()}
-                </FlipMove>
-                </div>
-                
-        );
-    }
+  }
+  render() {
+    return (
+      <div>
+        <FlipMove maintainContainerHeight={true}>
+          {this.renderPlayers()}
+        </FlipMove>
+      </div>
+    );
+  }
 };
 
-PlayerList.propTypes ={
-    players: PropTypes.array.isRequired
+PlayerList.propTypes = {
+  players: PropTypes.array.isRequired
 }
