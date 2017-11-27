f820a0a0408ddfeffe4ae054e19b685d530bf94f
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-11-27 11:16:59 -0500

    new folder

--- imports/ui/AddPlayer.js
+++ imports/ui/AddPlayer.js
@@ -1,30 +1,31 @@
 import React from 'react';
-
 import {Players} from './../api/players';
 
-export default class AddPlayer extends React.Component {
-  handleSubmit(e) {
-    let playerName = e.target.playerName.value;
 
-    e.preventDefault();
 
-    debugger;
-    if (playerName) {
-      e.target.playerName.value = '';
-      Players.insert({
-        name: playerName,
-        score: 0
-      });
+export default class AppPlayer extends React.Component {
+    handleSubmit(e){
+        let playerName = e.target.playerName.value;
+        e.preventDefault();
+     
+        if(playerName){
+            e.target.playerName.value = "";
+            Players.insert({
+                name: playerName,
+                score: 0
+            });
+        }
     }
-  }
-  render() {
-    return (
-      <div className="item">
-        <form className="form" onSubmit={this.handleSubmit.bind(this)}>
-          <input className="form__input" type="text" name="playerName" placeholder="Player name"/>
-          <button className="button">Add Player</button>
-        </form>
-      </div>
-    );
-  }
-};
+    render(){        
+        return(   
+            <div className="item">
+                <form className="form" onSubmit={this.handleSubmit.bind(this)}>
+                    <input className="form__input" type="text" name="playerName" placeholder="Player name"/>
+                    <button className="button">Add player</button>
+                </form>
+            
+            </div>        
+                            
+        );
+    }
+}
