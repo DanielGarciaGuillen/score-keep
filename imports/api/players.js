e78c2c2a13d0bd590666f1e3483c381022ded5a4
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-11-27 11:11:08 -0500

    change in folders

--- imports/api/players.js
+++ imports/api/players.js
@@ -4,18 +4,17 @@ import numeral from 'numeral';
 export const Players = new Mongo.Collection('players');
 
 export const calculatePlayerPositions = (players) => {
-    let rank =1; 
+  let rank = 1;
 
-    return players.map((player, index)=>{
-        if(index !==0 && players[index-1].score>player.score){
-            rank++;
-        }
+  return players.map((player, index) => {
+    if (index !== 0 && players[index - 1].score > player.score) {
+      rank++;
+    }
 
-        return {
-            ...player,
-            rank,
-            position: numeral(rank).format('0o')
-
-        };
-    });
-}
+    return {
+      ...player,
+      rank,
+      position: numeral(rank).format('0o')
+    };
+  });
+};
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import TitleBar from './TitleBar';
 import AddPlayer from './AddPlayer';
 import PlayerList from './PlayerList';
 
-
 export default class App extends React.Component {
-    render(){
-        return (
-            <div>              
-                    <TitleBar title={this.props.title} subtitle="Created by Daniel GG"/>            
-             <div className= "wrapper">
-                    <PlayerList players={this.props.players}/>
-                    <AddPlayer/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <TitleBar title={this.props.title} subtitle="Created by Andrew Mead"/>
+        <div className="wrapper">
+          <PlayerList players={this.props.players}/>
+          <AddPlayer/>
+        </div>
+      </div>
+    );
+  }
 };
 
 App.propTypes = {
-    title: PropTypes.string.isRequired,
-    players: PropTypes.array.isRequired
-}
+  title: React.PropTypes.string.isRequired,
+  players: React.PropTypes.array.isRequired
+};
